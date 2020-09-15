3a567e95de2acb71a62c960095c1dcf3a9da40f8
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-09-14 18:25:58 -0700

    lab 26 completed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import './App.scss';
+import Header from './components/Header';
+import Form from './components/Form';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  )
+
+ 
+}
+
+
+
+export default App;
