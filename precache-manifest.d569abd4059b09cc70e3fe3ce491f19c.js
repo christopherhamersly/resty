8732e05d5aa6e5596f0511d32ed49c12e86b2aa9
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-09-16 07:28:45 -0700

    Updates

---
+++ precache-manifest.d569abd4059b09cc70e3fe3ce491f19c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df806ddad166afe5d1a566e4c217bcfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b599a0bb0ed6fa22853",
+    "url": "/static/css/main.75c95ee7.chunk.css"
+  },
+  {
+    "revision": "9a6cdd7ad49509ac54e1",
+    "url": "/static/js/2.b35c5bdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b35c5bdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b599a0bb0ed6fa22853",
+    "url": "/static/js/main.7da54334.chunk.js"
+  },
+  {
+    "revision": "e7b4ea4f970612a52c32",
+    "url": "/static/js/runtime-main.3acf32b8.js"
+  }
+]);
